781218932035b54df79dd5a47019e32305101774
siddharthsingh91 <siddharthsingh91@gmail.com>
2014-02-05 17:38:35 +0530

    Wordpress Custom Page
    Wordpress custom page with custom header and menu

---
+++ plugin/custome-page-custome-file/home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Home page
+ * Develope by:siddharth singh
+ * Email:siddharthsingh91@gmail.com
+ * Author url:fileworld.in
+ * @link http://fileworld.in
+ *
+ * @package Siddharth Singh
+ * @subpackage Siddharth Custom page
+ * @since Custom Page 1.0
+ */
+?>
+<?php get_header( 'home' ); // Loads the header-home.php template.?>
+
+
+  <?php get_template_part( 'menu', 'primary' ); // Loads the menu-primary.php template. ?>
+
+
+ <?php get_footer();?>
