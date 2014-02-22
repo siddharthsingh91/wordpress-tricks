0b7fd0a4139084b47933a15de9695400e5c84be1
siddharthsingh91 <siddharthsingh91@gmail.com>
2014-02-22 18:02:51 +0530

    Plugin directory path
    Plugin directory path wordpress function is added.

---
+++ plugin/helping_function_for_wordpress_plugin/plugin_directory_path.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: plugin directory path
+ * Plugin URI: http://gaanaworld.com/
+ * Description: Work how to call plugin directory, below function tell how to call wordpress plugin directory image. 
+ * Version: 1.0
+ * Author: Siddharth Singh
+ * Author URI: http://gaanaworld.com/
+ * License: A "Slug" license name e.g. GPL2
+ */
+ ?>
+ <img src="<?php echo plugins_url('/images/close.png', __FILE__);?>
